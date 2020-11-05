7ad7e2fc6746a87ef3a23dd8b7f34b794ddfa77b
VENDREDl <node.js_@kakao.com>
2020-11-05 20:14:23 +0900

    Update GlobalStyles.js

--- src/Components/GlobalStyles.js
+++ src/Components/GlobalStyles.js
@@ -1,6 +1,5 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
-import styled from "styled-components";
 
 const globalStyle = createGlobalStyle`
     ${reset};
