7a179e15860c571ce55f4631ecfc4b45df22c16b
VENDREDl <65541546+VENDREDl@users.noreply.github.com>
2020-11-16 14:26:16 +0900

    Update App.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,22 +3,6 @@ import Router from "Components/Router";
 import GlobalStyles from "Components/GlobalStyles";
 import "./Fonts.css";
 import "./styles.css";
-import styled from "styled-components";
-
-const Btn = styled.button`
-  z-index: 5;
-  position: fixed;
-  right: 30px;
-  bottom: 30px;
-  width: 40px;
-  height: 40px;
-  background-color: #ffffff;
-  border-radius: 100px;
-  display: flex;
-  justify-content: center;
-  line-height: 35px;
-  border: 1px solid yellow;
-`
 
 function App() {
   return (
@@ -26,7 +10,7 @@ function App() {
       <Router />
       <GlobalStyles />
       <iframe class="frame" src="https://github.com/VENDREDl/dit1/blob/main/src/Components/App.js" title="myFrame"/>
-      <Btn>Chat</Btn>
+      <button>Chat</button>
     
     </>
   );
